d880f5b7bce6741b4bfb48544eca6e2d0dbf1a20
Rachel <46611223+rachelrly@users.noreply.github.com>
2023-09-19 13:18:12 -0500

    adds some logging stuff

--- checkout-file.sh
+++ checkout-file.sh
@@ -1,8 +1,11 @@
+GREEN='\033[0;32m'
+NC='\033[0m'
 branch="main"
 if [ $# -eq 2 ]
 then
 branch="$2"
 fi
+echo "${GREEN}Replacing $1 with version from $branch${NC} branch"
 git checkout $branch -- $1
 git add $1
 git commit -m "replacing $1 with version from $branch"

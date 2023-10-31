205f2c723aa33b6386b22cef4188b8755f98ee90
Rachel <46611223+rachelrly@users.noreply.github.com>
2023-10-31 15:16:51 -0500

    fixes text color for output in checkout-file.sh

--- checkout-file.sh
+++ checkout-file.sh
@@ -5,7 +5,7 @@ if [ $# -eq 2 ]
 then
 branch="$2"
 fi
-echo "${GREEN}Replacing $1 with version from $branch${NC} branch"
+echo "${GREEN}Replacing $1 with version from $branch branch${NC}"
 git checkout $branch -- $1
 git add $1
 git commit -m "replaces $1 with version from $branch"

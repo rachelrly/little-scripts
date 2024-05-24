44d863129db9ce1cd6bcf4f54c149625785f080f
Rachel <46611223+rachelrly@users.noreply.github.com>
2024-05-24 09:52:51 -0600

    vilolates newly red commit message by tweaking script and pushing directly to main 🤷‍♀️

--- commit.sh
+++ commit.sh
@@ -1,13 +1,15 @@
+RED='\033[0;31m'
+NC='\033[0m'
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$current_branch" = "main" ]; then
-    echo "Stop trying to push to the main branch, Rachel"
+    echo "\n${RED}Stop trying to push to the main branch, Rachel${NC}\n"
     exit 1
 fi
 
 if [ "$1" = "fmc" ] 
 then
-message="fixes merge conflict"
+message="merges in new main branch and fixes merge conflict"
 elif [ "$1" = "db" ]
 then
 rails db:migrate:with_data

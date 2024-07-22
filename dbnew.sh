7f5245c91a4c9b92b855c581b7e6f90cf050ff88
Rachel <46611223+rachelrly@users.noreply.github.com>
2024-07-22 10:08:54 -0600

    makes dbnew output pretty

--- dbnew.sh
+++ dbnew.sh
@@ -1,6 +1,10 @@
+GREEN='\033[0;32m'
+NC='\033[0m'
+echo "\n${GREEN}Dropping and recreating rails database${NC}\n"
 rails db:drop
 rails db:create
 rails db:migrate:with_data
-echo 'Deleting log files'
+echo "\n${GREEN}Deleting log files${NC}\n"
 rm test.log development.log delayed_job.log
+echo "\n${GREEN}Enabling all feature flags${NC}\n"
 bash script/enable_feature_flags.sh
